dcd455f83b8df9361c840379978a4dc9a70905d8
balarabeel-sudo <balarabeel@gmail.com>
2026-09-18 12:43:08 +0100

    Create Icons.tsx

---
+++ Icons.tsx
@@ -0,0 +1,79 @@
+type IconProps = {
+  name: string
+  size?: number
+  color?: string
+  strokeWidth?: number
+}
+
+const paths: Record<string, string> = {
+  home: 'M3 11l9-8 9 8M5 10v10a1 1 0 0 0 1 1h4v-6h4v6h4a1 1 0 0 0 1-1V10',
+  cart: 'M3 3h2l.4 2M7 13h10l3-8H5.4M7 13L5.4 5M7 13l-1.3 4.3A1 1 0 0 0 6.7 19H18M9 21a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm9 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z',
+  building: 'M4 21V5a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v16M12 21v-6a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v6M8 7h.01M8 11h.01M8 15h.01M4 21h16',
+  robot: 'M9 7V5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2M5 7h14a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2Zm3 5h.01M16 12h.01M9 17h6',
+  users: 'M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8Zm11 10v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75',
+  tractor: 'M3 17a3 3 0 1 0 6 0 3 3 0 0 0-6 0Zm12 0a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM6 17h6M9 17V9h4l3 4h2v4M9 9V5H6',
+  bookmark: 'M6 3h12a1 1 0 0 1 1 1v17l-7-4-7 4V4a1 1 0 0 1 1-1Z',
+  search: 'M11 19a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm10 2-4.35-4.35',
+  bell: 'M18 8a6 6 0 1 0-12 0c0 7-3 9-3 9h18s-3-2-3-9Zm-7 13a2 2 0 0 0 4 0',
+  message: 'M21 15a2 2 0 0 1-2 2H8l-5 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10Z',
+  user: 'M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z',
+  heart: 'M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8l1 1L12 21l7.8-7.6 1-1a5.5 5.5 0 0 0 0-7.8Z',
+  comment: 'M21 11.5a8.4 8.4 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.4 8.4 0 0 1-3.8-.9L3 21l1.9-5.7a8.4 8.4 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.4 8.4 0 0 1 3.8-.9h.5a8.5 8.5 0 0 1 8 8v.5Z',
+  share: 'M4 12v7a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7M16 6l-4-4-4 4M12 2v13',
+  chevronRight: 'M9 18l6-6-6-6',
+  chevronLeft: 'M15 18l-6-6 6-6',
+  arrowLeft: 'M19 12H5M12 19l-7-7 7-7',
+  arrowRight: 'M5 12h14M12 5l7 7-7 7',
+  plus: 'M12 5v14M5 12h14',
+  close: 'M18 6 6 18M6 6l12 12',
+  edit: 'M11 4H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-5M18.5 2.5a2.1 2.1 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5Z',
+  lock: 'M5 11h14v10H5V11Zm3 0V7a4 4 0 0 1 8 0v4',
+  globe: 'M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm-9-9h18M12 3a14 14 0 0 1 4 9 14 14 0 0 1-4 9 14 14 0 0 1-4-9 14 14 0 0 1 4-9Z',
+  currency: 'M17 5H9.5a3.5 3.5 0 0 0 0 7h1a3.5 3.5 0 0 1 0 7H4M7 19h8M12 2v2M12 20v2',
+  moon: 'M21 12.8A9 9 0 1 1 11.2 3 7 7 0 0 0 21 12.8Z',
+  crown: 'M3 8l4 4 5-7 5 7 4-4-2 11H5L3 8Zm2 13h14',
+  leaf: 'M11 20A7 7 0 0 1 4 13c0-6 7-11 15-11 0 8-5 15-11 15a7 7 0 0 1-6-3.5',
+  mapPin: 'M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z',
+  star: 'M12 2l2.9 6.6L22 9.3l-5 4.9 1.2 7.2L12 17.8l-6.2 3.6L7 14.2 2 9.3l7.1-.7L12 2Z',
+  menu: 'M4 6h16M4 12h16M4 18h16',
+  wrench: 'M14.7 6.3a4 4 0 0 1 5 5l-6.1 6.1a2 2 0 0 1-2.8 0L4.6 11.2a2 2 0 0 1 0-2.8l6.1-6.1a4 4 0 0 1 4 4Z',
+  truck: 'M2 8h11v8H2V8Zm11 3h4l3 3v2h-7v-5Zm-8 8a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm11 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z',
+  seedling: 'M12 21v-8M12 13c0-4-3-7-7-7 0 4 3 7 7 7Zm0 0c0-5 3-9 8-9 0 5-3 9-8 9Z',
+  droplet: 'M12 2s6 7 6 12a6 6 0 0 1-12 0c0-5 6-12 6-12Z',
+  checkCircle: 'M22 11.1V12a10 10 0 1 1-6-9.2M22 4 12 14.1l-3-3',
+  filter: 'M4 4h16l-6 8v6l-4 2v-8L4 4Z',
+  camera: 'M4 8h3l1.5-2h7L17 8h3a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1Zm8 3a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z',
+  send: 'M22 2 11 13M22 2l-7 20-4-9-9-4 20-7Z',
+  logout: 'M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9',
+  refresh: 'M23 4v6h-6M1 20v-6h6M3.5 9a9 9 0 0 1 15-4.5L23 9M1 15l4.5 4.5A9 9 0 0 0 20.5 15',
+  alertTriangle: 'M10.3 3.9 1.8 18a2 2 0 0 0 1.7 3h17a2 2 0 0 0 1.7-3L13.7 3.9a2 2 0 0 0-3.4 0ZM12 9v4M12 17h.01',
+}
+
+export default function Icon({ name, size = 20, color = 'currentColor', strokeWidth = 2 }: IconProps) {
+  const d = paths[name]
+
+  if (!d) {
+    return (
+      <svg width={size} height={size} viewBox="0 0 24 24" fill="none">
+        <circle cx="12" cy="12" r="9" stroke={color} strokeWidth={strokeWidth} />
+      </svg>
+    )
+  }
+
+  const filled = name === 'heart' || name === 'star'
+
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d={d} fill={filled ? 'none' : 'none'} />
+    </svg>
+  )
+}
